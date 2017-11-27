f333176d0736ad82739cd23d8c1a9daa46cf9e06
Nayrton <nayrton1999@gmail.com>
2017-11-27 03:54:37 -0200

    index version:0.1

--- index.php
+++ index.php
@@ -1,13 +1,172 @@
 <!DOCTYPE html>
-
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>South América Movies</title>
+        <link href="css/stile.css" rel="stylesheet"/>
+        <script src="script.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="slick/slick.css"/>        
+        <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
     </head>
-    <body>
+    <body>        
+        <main>
+            <header class="fixo">
+                <a href="index.php"><img src="imgs/SAM.png" width="100" height="100" alt="SAM"></a> 
+                <ul>
+                    <li class="radios"><a href="index.php" >Inicio</a></li>
+                    <li class="radios"><a href="testecarroseu.php">Lista</a></li>
+                    <li class="radios"><a href="melhores-sites.php">Melhores Sites</a></li>
+                    <li class="radios">
+                        <form>
+                            <input class="seachimput radios" placeholder=" Pesquise Aqui!" type="text">
+                        </form>
+
+                    </li>
+                </ul>                          
+            </header>
+            <div class="principal">
+                <div class="baners">
+                    <a href="filmes/thegamerule.php"><img src="imgs/testebaner.jpg" alt="testebaner"/></a>
+                    <a href="filmes/thegamerule.php"><img src="imgs/testebaner.jpg" alt="testebaner"/></a>
+                    <a href="filmes/thegamerule.php"><img src="imgs/testebaner.jpg" alt="testebaner"/></a>
+                    <a href="filmes/thegamerule.php"><img src="imgs/testebaner.jpg" alt="testebaner"/></a>
+                    <a href="filmes/thegamerule.php"><img src="imgs/testebaner.jpg" alt="testebaner"/></a>
+                    <a href="filmes/thegamerule.php"><img src="imgs/testebaner.jpg" alt="testebaner"/></a>
+                </div>
+                <div class="contentfilmes ">
+                    <a class="contentfilmestitulo" href="#">Recomendado</a>
+                    <div class="teste" data-slick='{"slidesToShow": 6, "slidesToScroll": 4}'>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>                
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>   
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+
+                    </div>
+                </div>
+                <div class="contentfilmes ">
+                    <a class="contentfilmestitulo" href="#">teste1</a>
+                    <div class="teste" data-slick='{"slidesToShow": 6, "slidesToScroll": 4}'>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>                
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>   
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+
+                    </div>
+                </div>
+                <div class="contentfilmes ">
+                    <a class="contentfilmestitulo" href="#">teste2</a>
+                    <div class="teste" data-slick='{"slidesToShow": 6, "slidesToScroll": 4}'>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>                
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>   
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+
+                    </div>
+                </div>
+                <div class="contentfilmes ">
+                    <a class="contentfilmestitulo" href="#">teste3</a>
+                    <div class="teste" data-slick='{"slidesToShow": 6, "slidesToScroll": 4}'>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>                
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>   
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+
+                    </div>
+                </div>
+                <div class="contentfilmes ">
+                    <a class="contentfilmestitulo" href="#">teste4</a>
+                    <div class="teste" data-slick='{"slidesToShow": 6, "slidesToScroll": 4}'>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>                
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>   
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+
+                    </div>
+                </div>
+                <div class="contentfilmes ">
+                    <a class="contentfilmestitulo" href="#">teste5</a>
+                    <div class="teste" data-slick='{"slidesToShow": 6, "slidesToScroll": 4}'>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>                
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>   
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/testeslider.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+                        <div><img src="imgs/0045402_200.jpg" alt=""/></div>
+
+                    </div>
+                </div>
+
+            </div>
+            <div class="footer">
+                <p>
+		Descubra novos animes, e se divirta Assistindo! <br>
+		TopAnimeList1.0 © 2017. All rights reserved. Designed by <em>Nayrton Bertoldo.</em>
+		</p>
+            </div>
+        </main>
         <?php
-        // put your code here
         ?>
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="slick/slick.min.js"></script>
+        <script src="script.js" type="text/javascript"></script>
     </body>
 </html>
